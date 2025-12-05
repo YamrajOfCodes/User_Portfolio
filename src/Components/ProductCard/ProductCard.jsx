497ef00284834155981e1eceed8ed0aadb4e21f6
YamrajOfcodes <kundanpatil0111@.gmail.com>
2025-12-05 12:55:52 +0530

    increase fontSize of project heading

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -3,7 +3,7 @@
     <div className="flex justify-start p-4 rounded-xl w-full sm:w-1/2 lg:w-1/3 2xl:w-1/4 ">
       <div className="bg-bg-primary p-6 w-full border flex flex-col gap-2 border-Border rounded-xl">
         <div className="w-full h-60 rounded-2xl bg-gradient-to-br from-pink-500 via-purple-500 to-blue-300 mb-4"></div>
-        <h2 className="text-white text-xl xl:text-2xl font-semibold mb-3">
+        <h2 className="text-white text-xl xl:text-3xl font-semibold mb-3">
          {projectName || "Project"}
         </h2>
         <div className="flex flex-wrap gap-2 mb-4">
